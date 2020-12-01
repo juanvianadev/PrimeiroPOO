b5008977ec002bb20622469ae1bf0350776f4d44
Juan <juanviana2577@gmail.com>
2020-12-01 14:14:15 -0300

    Atributos e Metodos Finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem1 = new Personagem();
+
+            objetoPersonagem1.nome = "Juanzada";
+            objetoPersonagem1.idade = 17;
+            objetoPersonagem1.nome = "Força";
+
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem1.nome = "Metaforando";
+            objetoPersonagem1.idade = 999;
+            objetoPersonagem1.nome = "Mental";
+
+
+        }
+    }
+}
